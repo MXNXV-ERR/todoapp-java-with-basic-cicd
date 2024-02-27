2fd82378b7b2c5a282889023b630249944d423ee
MXNXV-ERR <angbymtg@gmail.com>
2024-02-27 13:45:28 +0530

    testsetsetsetest

--- src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
@@ -18,32 +18,32 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 @SpringBootTest
 public class TodoappApplicationTests {
 
-	@Autowired
-	private static WebDriver chrome=new ChromeDriver();
+	// @Autowired
+	// private static WebDriver chrome=new ChromeDriver();
 
-	@Bean
-	public WebDriver chrome()
-	{
-		System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-		return new ChromeDriver();
-	}
-	@BeforeAll
-	public static void setUp()
-	{
-		chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-	}
+	// @Bean
+	// public WebDriver chrome()
+	// {
+	// 	System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+	// 	return new ChromeDriver();
+	// }
+	// @BeforeAll
+	// public static void setUp()
+	// {
+	// 	chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	// }
 
-	@AfterAll
-	public static void tearDown()
-	{
-		chrome.quit();
+	// @AfterAll
+	// public static void tearDown()
+	// {
+	// 	chrome.quit();
 		
-	}
+	// }
 	
-	@Test
-	void contextLoads() {
-		System.setProperty("webdriver.chrome.verboseLogging", "true");
+	// @Test
+	// void contextLoads() {
+	// 	System.setProperty("webdriver.chrome.verboseLogging", "true");
 
-		chrome.get("http://127.0.0.1:8091/");
-	}
+	// 	chrome.get("http://127.0.0.1:8091/");
+	// }
 }
