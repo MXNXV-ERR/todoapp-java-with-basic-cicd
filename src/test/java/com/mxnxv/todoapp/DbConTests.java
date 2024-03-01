baab285070788d23fdf5472d7bb8f4ce47b95f48
MXNXV-ERR <manavrai.jmn@gmail.com>
2024-03-01 14:18:32 +0530

    somethingwaswrong

---
+++ src/test/java/com/mxnxv/todoapp/DbConTests.java
@@ -0,0 +1,45 @@
+package com.mxnxv.todoapp;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest
+@SpringJUnitConfig
+@Configuration
+@TestPropertySource("classpath:application.properties")
+public class DbConTests {
+    
+
+    Logger log = LoggerFactory.getLogger(DbConTests.class);
+
+    @Value("${spring.datasource.url}")
+    String dbUrl;
+
+    @Test
+    void dbConTest()
+    {
+        try {
+            Connection con= DriverManager.getConnection(dbUrl);
+            assertTrue(con.isValid(5));
+            con.isValid(5);
+            log.info("Connection checked✅ .... Closing Connection...");
+            con.close();
+        } catch (SQLException e) {
+            log.error( e.getMessage());
+           
+        }
+    }
+}
--- src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
@@ -1,13 +1,11 @@
 package com.mxnxv.todoapp;
 
-import java.time.Duration;
-
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,32 +16,30 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 @SpringBootTest
 public class TodoappApplicationTests {
 
-	// @Autowired
-	// private static WebDriver chrome=new ChromeDriver();
+	TodoappApplicationTests(){SpringApplication.run(TodoappApplication.class);}
 
-	// @Bean
-	// public WebDriver chrome()
-	// {
-	// 	System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-	// 	return new ChromeDriver();
-	// }
+	@Autowired
+	private static WebDriver chrome=new ChromeDriver();
+
+	@Bean
+	public WebDriver chrome()
+	{
+		System.setProperty("webdriver.chrome.driver","drivers/chromedriver_latest.exe");
+		return new ChromeDriver();
+	}
 	// @BeforeAll
 	// public static void setUp()
 	// {
 	// 	chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 	// }
 
-	// @AfterAll
-	// public static void tearDown()
-	// {
-	// 	chrome.quit();
-		
-	// }
+	@AfterAll
+	public static void tearDown(){chrome.quit();}
 	
-	// @Test
-	// void contextLoads() {
-	// 	System.setProperty("webdriver.chrome.verboseLogging", "true");
-
-	// 	chrome.get("http://127.0.0.1:8091/");
-	// }
+	@Test
+	void contextLoads() {
+		// System.setProperty("webdriver.chrome.verboseLogging", "true");
+		chrome.manage().window().maximize();
+		chrome.get("http://127.0.0.1:8091/");
+	}
 }
