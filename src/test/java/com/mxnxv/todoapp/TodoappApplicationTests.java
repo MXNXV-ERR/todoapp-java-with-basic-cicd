9f2af69af2e85b6de22b5d7b032418df1a60f468
MXNXV-ERR <manavrai.jmn@gmail.com>
2024-03-08 21:31:31 +0530

    So to do testing u don`t need head

--- src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
+++ src/test/java/com/mxnxv/todoapp/TodoappApplicationTests.java
@@ -1,12 +1,14 @@
 package com.mxnxv.todoapp;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
@@ -18,45 +20,31 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 @SpringBootTest
 public class TodoappApplicationTests {
 
+	static ChromeDriver chrome ;
+
 	TodoappApplicationTests(){SpringApplication.run(TodoappApplication.class);}
 
-	@Autowired
-	private static WebDriver chrome=new ChromeDriver();
-
-	@Bean
-	public WebDriver chrome()
-	{
-		System.setProperty("webdriver.chrome.driver","drivers/chromedriver_latest.exe");
-		return new ChromeDriver();
-	}
-	// @BeforeAll
-	// public static void setUp()
-	// {
-	// 	chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-	// }
-
-	@AfterAll
-	public static void tearDown(){chrome.quit();}
+		@BeforeAll
+		public static void load(){
+				ChromeOptions opts = new ChromeOptions();
+				opts.addArguments("--headless");
+				chrome =  new ChromeDriver(opts);
+		}
 	
-	@Test
-	void contextLoads() {
-		// System.setProperty("webdriver.chrome.verboseLogging", "true");
-		chrome.manage().window().maximize();
-		chrome.get("http://127.0.0.1:8091/");
-		WebElement createNew = chrome.findElement(By.id("Add-TODO-Item-Buttom"));
-		createNew.click();
-		WebElement title = chrome.findElement(By.id("title"));
-		title.sendKeys("Test Todo");
-		WebElement date = chrome.findElement(By.id("date"));
-		date.sendKeys("01-01-2014");
-		WebElement status = chrome.findElement(By.id("status"));
-		status.sendKeys("Not Completed");
-		WebElement saveButton = chrome.findElement(By.id("SaveButton"));
-		saveButton.click();
-
+		@AfterAll
+		public static void tearDown(){chrome.quit();}
 		
-		
-
-
+		@Test
+		void contextLoads() {
+			chrome.manage().window().maximize();
+			chrome.get("http://127.0.0.1:8091/");
+			WebElement createNew = chrome.findElement(By.id("Add-TODO-Item-Buttom"));
+			createNew.click();
+			WebElement title = chrome.findElement(By.id("title"));
+			title.sendKeys("Test Todo");
+			WebElement date = chrome.findElement(By.id("date"));
+			date.sendKeys("01-01-2014");
+			WebElement saveButton = chrome.findElement(By.id("SaveButton"));
+			saveButton.click();
 	}
 }
